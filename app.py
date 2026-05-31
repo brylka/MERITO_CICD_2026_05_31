e55a7345af38c5f5c9795f145549258c9d4a979b
Bartosz Bryniarski <brylka@jelenia.com>
2026-05-31 12:20:02 +0200

    Procesy CI/CD aplikacji webowej na azure

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
 
+load_dotenv()
+
+test = os.environ.get('TEST', 'brak zmiennej')
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Witaj MERITO!</h1><p>Autor: Bartosz Bryniarski</p>'
+    return f'<h1>Witaj MERITO!</h1><p>Zmienna TEST = {test}</p><p>Autor: Bartosz Bryniarski</p>'
 
 
 if __name__ == '__main__':
