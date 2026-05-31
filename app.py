e3e0bf0b92a12e90c1484ff34486d7bac61fe86f
Bartosz Bryniarski <brylka@jelenia.com>
2026-05-31 14:20:40 +0200

    Procesy CI/CD aplikacji webowej na azure

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
 
 test = os.environ.get('TEST', 'brak zmiennej')
+GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY', 'brak zmiennej')
+
+client = genai.Client()
+
+history = []
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return f'<h1>Witaj MERITO!</h1><p>Zmienna TEST = {test}</p><p>Autor: Bartosz Bryniarski</p>'
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+        history.append({"role": "user", "parts": [{"text": prompt}]})
+        response = client.models.generate_content(
+            model="gemini-3.5-flash",
+            contents=history
+        )
+        history.append({"role": "model", "parts": [{"text": response.text}]})
+
+    return render_template('index.html', history=history)
 
 
 if __name__ == '__main__':
